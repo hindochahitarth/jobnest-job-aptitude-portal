786e918f70a510b9a5c87a2d14ffdc26d32495a1
Hitarth Hindocha <hindochahitarth@gmail.com>
2026-07-19 13:40:27 +0530

    Initialize Project Structure and Configuration Files

---
+++ src/main/java/org/miniproject/jobnestjobaptitudeportal/JobnestJobAptitudePortalApplication.java
@@ -0,0 +1,13 @@
+package org.miniproject.jobnestjobaptitudeportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobnestJobAptitudePortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobnestJobAptitudePortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/miniproject/jobnestjobaptitudeportal/JobnestJobAptitudePortalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.miniproject.jobnestjobaptitudeportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobnestJobAptitudePortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
